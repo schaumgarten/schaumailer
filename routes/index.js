0fe5e0aede37b3d101c8820f55feab7ed55545d0
Schaum <schaumgarten@gmail.com>
2018-10-16 21:56:00 -0500

    github

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const mailer = require('../helpers/mailer');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('forms');
 });
 
 router.post('/', (req, res) => {
